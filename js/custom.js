fb622ae192cdc93ddd8b4a5bcc80abec56783f3d
alainncls <alain.nicolas.perso@gmail.com>
2020-12-15 00:33:56 +0100

    Mise à jour du Tue Dec 15 00:33:56 CET 2020

--- js/custom.js
+++ js/custom.js
@@ -9,23 +9,25 @@ function showModal(modalId, imgSrc) {
         document.body.style.top = `-${window.scrollY}px`;
         document.body.style.position = 'fixed';
         modal.style.display = 'block';
-    }
 
-    console.log('touchEvent', touchEvent)
-    if (touchEvent === 'click') {
-        modal.onclick = () => hideModal(modal);
-        closeButton.onclick = () => hideModal(modal);
-    } else {
-        modal.ontouchstart = () => hideModal(modal);
-        closeButton.ontouchstart = () => hideModal(modal);
+        console.log('touchEvent')
+        if (touchEvent === 'click') {
+            modal.onclick = () => hideModal(modal);
+            closeButton.onclick = () => hideModal(modal);
+        } else {
+            modal.ontouchstart = () => hideModal(modal);
+            closeButton.style.display = 'none';
+        }
     }
 }
 
 function hideModal(modal) {
-    console.log('hide')
-    const scrollY = document.body.style.top;
-    document.body.style.position = '';
-    document.body.style.top = '';
-    window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    modal.style.display = 'none';
+    if (modal.style.display === 'block') {
+        console.log('hide')
+        const scrollY = document.body.style.top;
+        document.body.style.position = '';
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+        modal.style.display = 'none';
+    }
 }
